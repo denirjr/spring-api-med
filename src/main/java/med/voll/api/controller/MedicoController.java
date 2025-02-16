04e490ba6c586e9ef357f3630a15684c7a097e1a
denirjr <denirdamasceno7@gmail.com>
2025-02-16 19:39:53 -0300

    wip(master): created pagination

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,10 +6,11 @@ import med.voll.api.medico.DadosListagemMedicoDTO;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -25,7 +26,7 @@ public class MedicoController {
     }
 
     @GetMapping()
-    public List<DadosListagemMedicoDTO> listarMedicos() {
-        return repository.findAll().stream().map(DadosListagemMedicoDTO::new).toList();
+    public Page<DadosListagemMedicoDTO> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedicoDTO::new);
     }
 }
